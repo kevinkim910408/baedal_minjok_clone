5436c069940df2cc701bb74faff611c65851305f
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-06-17 19:24:03 -0400

    feat: 아이디 정규식 적용, 회원가입시 로그인페이지 이동

--- src/Shared/LoginCheck.js
+++ src/Shared/LoginCheck.js
@@ -1,19 +1,19 @@
 // 로그인, 회원가입 유효성 검사 파일
 
 // 아이디 형식 검사
-export const idCheck = (username) => {
-  const regPass = /^(?=.*[0-9a-zA-Z])[가-힣a-zA-Z0-9-_.]{4,15}$/;
-  return regPass.test(username);
+export const idRegCheck = (id) => {
+  const regPass = /^(?=.*[0-9a-zA-Z가-힣])[가-힣a-zA-Z0-9-_.]{4,15}$/;
+  return regPass.test(id);
 };
 
 // 비밀번호 체크
-export const passwordCheck = (passwordcheck) => {
+export const passwordRegCheck = (pw) => {
   const regPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,20}$/;
-  return regPass.test(passwordcheck);
+  return regPass.test(pw);
 };
 
 export const emailRegCheck = (email) =>{
-  const regPass = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
+  const regPass = /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
   return regPass.test(email);
 }
 
