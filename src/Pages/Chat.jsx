abcd7fb1d9d07c06a136d5190adcd048904038fa
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-06-19 11:45:41 -0400

    feat: 채팅 작업중..

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -5,28 +5,34 @@ import flex from '../Components/Common/flex'
 import ChatPic from '../Assets/Image/ChatLogo.png'
 import Mainheader from '../Components/MainComponents/Mainheader'
 
-const socket = io.connect("http://3.37.87.166");
+const socket = io.connect("http://3.37.87.166/chat");
 
 const Chat = () => {
-    const [sendMSG, setSendMSG] = useState("");
-    const [receiveMSG, setReceiveMSG] = useState("");
     const chatRef = useRef();
-
-    const onSendMSGHandler = (e) => {
-        e.preventDefault();
-        socket.emit("chat", {msg: sendMSG});
-        chatRef.current.value = "";
-    }
+    const [flag, setFlag] = useState(true);
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [messageSends, setMessageSends] = useState([]);
+    const token = localStorage.getItem("Authorization")
 
     useEffect(()=>{
-        setSendMSG();
         chatRef.current.focus();
-        socket.on("chat", (data)=>{
-            setReceiveMSG(data);
-        })
-    },[socket])
 
-    console.log(sendMSG, receiveMSG)
+        if(flag){
+            socket.emit("token", {token:`Bearer ${token}`} );
+            setFlag(false)
+        }
+        socket.on("chat", (data)=>{
+            setMessages([...messages, data])
+        })
+    },[flag, token, messages])
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message !== "") socket.emit("chat", {msg: message}, () => setMessage(''));
+        setMessageSends([...messageSends, chatRef.current.value])
+        chatRef.current.value = "";
+    }
 
     return (
         <StWrap> 
@@ -34,12 +40,22 @@ const Chat = () => {
             <StImg src={ChatPic} alt="" />
             <StTitle>무엇을 도와드릴까요?</StTitle>
             <StDiv>
-                {receiveMSG}
-                <StLine />
-                {sendMSG}
+                <div style={{width:'100%'}}>
+                    {/* {messages.map((v,i)=>{
+                        return <StPTwo>{v}</StPTwo> 
+                    })}
+                    {messageSends.map((v)=>{
+                        return <StP>{v}</StP>
+                    })} */}
+                </div>
                 <StChatDiv> 
-                    <StInput ref={chatRef} onChange={(e)=>setSendMSG(e.target.value)}/>
-                    <StButton onClick={(e)=>onSendMSGHandler(e)}>전송</StButton>
+                    <StLine />
+                    <input 
+                        type="text" 
+                        onChange={(event)=> setMessage(event.target.value)}
+                        onKeyDown={event=>event.key === 'Enter' ? sendMessage(event) : null}
+                        ref={chatRef}
+                    />
                 </StChatDiv>
             </StDiv>
         </StWrap>
@@ -58,7 +74,7 @@ const StWrap = styled.div`
     ${flex({direction:'column'})}
     height: 100vh;
     background-color: var(--white);
-    font-family: 'BMDOHYEON';
+    overflow: auto;
     & > .pass {
       color: var(--primary);
     }
@@ -68,27 +84,13 @@ const StWrap = styled.div`
 `;
 
 const StDiv = styled.div`
-    ${flex({direction:'column', justify:'flex-end'})}
+    ${flex({direction:'column', })}
     width: calc(100vh - 60vh);
     height: 800px;
     border: 2px solid var(--primary);
     border-radius: 10px;
     margin-bottom: 2rem;
-`;
-
-const StInput = styled.input`
-    width: 70%;
-    height: 30px;
-    border: none;
-    border-bottom: 2px solid var(--primary);
-    margin-bottom: 1rem;
-`;
-
-const StButton = styled.button`
-    width: 15%;
-    padding: 0.4rem;
-    border-radius: 10px;
-    margin-bottom: 1rem;
+    overflow: scroll;
 `;
 
 const StImg = styled.img`
@@ -105,11 +107,23 @@ const StLine = styled.div`
 `;
 
 const StChatDiv = styled.form`
-    ${flex({})}
+    ${flex({direction:'column', justify:'flex-end'})}
     width: 100%;
+    height: 100%;
 `;
 
 const StTitle = styled.div`
     font-size: 2rem;
     margin: 1rem;
+    font-family: 'BMDOHYEON';
+`;
+
+const StP = styled.div`
+    ${flex({ justify:'flex-end'})}
+    width: 100%;
+`;
+
+const StPTwo = styled.div`
+    ${flex({ justify:'flex-start'})}
+    width: 100%;
 `;
