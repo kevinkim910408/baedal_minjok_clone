3c12fb6e2e1d0717a246c4e63bffc9be7b6d6f4f
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-06-25 07:00:50 -0400

    feat: 회원가입 기능

--- src/Components/SignInUpComponents/SignupComp.jsx
+++ src/Components/SignInUpComponents/SignupComp.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { __userSignUp, __userIdCheck, __userEmailCheck } from '../../Redux/modules/user'
-import { passwordRegCheck, emailRegCheck, idRegCheck } from '../../Shared/LoginCheck'
-import {StInput, StWrap, StSmallInput , StSubmitButton, StDiv, StTitle, StFlexInfoInput, StButton} from './SigninupStyled'
-import ExitHeader from '../Common/ExitHeader'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  __userSignUp,
+  __userIdCheck,
+  __userEmailCheck,
+} from "../../Redux/modules/user";
+import {
+  passwordRegCheck,
+  emailRegCheck,
+  idRegCheck,
+} from "../../Shared/LoginCheck";
+import {
+  StInput,
+  StWrap,
+  StSmallInput,
+  StSubmitButton,
+  StDiv,
+  StTitle,
+  StFlexInfoInput,
+  StButton,
+} from "./SigninupStyled";
+import ExitHeader from "../Common/ExitHeader";
 
 const NICKNAME_MAX_LENGTH = 6;
 const EMAIL_MAX_LENGTH = 25;
@@ -22,78 +39,132 @@ const Signup = () => {
   const [savedId, setSavedId] = useState("");
   const [savedEmail, setSavedEmail] = useState("");
 
-  const {idCheck, emailCheck} = useSelector(state=>state.userReducer); // get data from Store
+  const { idCheck, emailCheck } = useSelector((state) => state.userReducer); // get data from Store
 
   // 버튼 비활성화 함수들
   const onDisableHandler = () => {
-    if(passwordRegCheck(pw) === false) return true;
+    if (passwordRegCheck(pw) === false) return true;
     else if (passwordRegCheck(pwTwo) === false) return true;
-    else if((Boolean(!email) || Boolean(!nickName) || Boolean(pw?.length < PW_MIN_LENGTH) || Boolean(pwTwo?.length < PW_MIN_LENGTH))) return true;
+    else if (
+      Boolean(!email) ||
+      Boolean(!nickName) ||
+      Boolean(pw?.length < PW_MIN_LENGTH) ||
+      Boolean(pwTwo?.length < PW_MIN_LENGTH)
+    )
+      return true;
     else if (pw !== pwTwo) return true;
     else if (idCheck === false) return true;
     else if (emailCheck === false) return true;
     else return false;
-  }
+  };
 
   const onDupIdDisableHandler = () => {
-    if(idRegCheck(nickName)=== false) return true;
-    if(idCheck === false)return false;
-    if(nickName !== savedId) return false;
+    if (idRegCheck(nickName) === false) return true;
+    if (idCheck === false) return false;
+    if (nickName !== savedId) return false;
     else return true;
-  }
+  };
 
   const onDupEmailDisableHandler = () => {
-    if(emailRegCheck(email) === false) return true;
-    if(emailCheck === false) return false;
-    if(email !== savedEmail) return false;
+    if (emailRegCheck(email) === false) return true;
+    if (emailCheck === false) return false;
+    if (email !== savedEmail) return false;
     else return true;
-  }
+  };
 
   // 서버 연결 함수들
   const onSignUpHandler = () => {
-    dispatch(__userSignUp({email,nickName,pw,pwTwo})); 
-    alert('회원가입이 완료되었습니다.')
-    navigate('/signin')
-  }
-  const onIdDupCheckHandler = (id) => {dispatch(__userIdCheck({id})); setSavedId(nickName)}
-  const onEmailDupCheckHandler = (email) => {dispatch(__userEmailCheck({email})); setSavedEmail(email);}
+    dispatch(__userSignUp({ email, nickName, pw, pwTwo }));
+    alert("회원가입이 완료되었습니다.");
+    navigate("/signin");
+  };
+  const onIdDupCheckHandler = (id) => {
+    dispatch(__userIdCheck({ id }));
+    setSavedId(nickName);
+  };
+  const onEmailDupCheckHandler = (email) => {
+    dispatch(__userEmailCheck({ email }));
+    setSavedEmail(email);
+  };
 
   return (
     <>
       <StWrap>
-          <ExitHeader text="signin"/>
-          {/* 텍스트 */}
-          <StTitle>회원가입</StTitle>
-          <StFlexInfoInput>
-            <span>필수 정보를 입력해주세요.</span>
-          </StFlexInfoInput>
-          {/* Inputs */}
-          <StDiv>
-            <StSmallInput type="text" placeholder='닉네임*(2-6글자)' onChange={(e)=> setNickName(e.target.value)} maxLength={NICKNAME_MAX_LENGTH}/>
-            <StButton disabled={onDupIdDisableHandler()} onClick={()=>{onIdDupCheckHandler(nickName)}}>중복체크</StButton>
-          </StDiv>
-
-          <StDiv>
-            <StSmallInput type="email" placeholder='아이디*(이메일형식)' onChange={(e)=> setEmail(e.target.value)} maxLength={EMAIL_MAX_LENGTH}/>
-            <StButton disabled={onDupEmailDisableHandler()} onClick={()=>{onEmailDupCheckHandler(email)}}>중복체크</StButton>
-          </StDiv>
-
-          <StInput type="password" placeholder='비밀번호' onChange={(e)=> setPw(e.target.value)} maxLength={PW_MAX_LENGTH}/>
-          {passwordRegCheck(pw) ? <p className='pass'>형식에 맞는 비밀번호 입니다.</p> : <p className='danger'> 영문+숫자 8-20자로 만들어 주세요.</p>}
-
-          <StInput type="password" placeholder='비밀번호 재확인' onChange={(e)=> setPwTwo(e.target.value)} maxLength={PW_MAX_LENGTH}/>
-          {pw === pwTwo && pwTwo?.length > PW_MIN_LENGTH ? <p className='pass'>형식에 맞는 비밀번호 입니다.</p> : <p className='danger'> 비밀번호가 일치하지 않거나 공백입니다.</p>}
-          
-          <StSubmitButton 
-            as='button' 
-            onClick={onSignUpHandler} 
-            disabled={onDisableHandler()}
+        <ExitHeader text="signin" />
+        {/* 텍스트 */}
+        <StTitle>회원가입</StTitle>
+        <StFlexInfoInput>
+          <span>필수 정보를 입력해주세요.</span>
+        </StFlexInfoInput>
+        {/* Inputs */}
+        <StDiv>
+          <StSmallInput
+            type="text"
+            placeholder="닉네임*(2-6글자)"
+            onChange={(e) => setNickName(e.target.value)}
+            maxLength={NICKNAME_MAX_LENGTH}
+          />
+          <StButton
+            disabled={onDupIdDisableHandler()}
+            onClick={() => {
+              onIdDupCheckHandler(nickName);
+            }}
           >
-            가입하기
-          </StSubmitButton>
-        </StWrap>
-    </>
-  )
-}
+            중복체크
+          </StButton>
+        </StDiv>
 
-export default Signup
+        <StDiv>
+          <StSmallInput
+            type="email"
+            placeholder="아이디*(이메일형식)"
+            onChange={(e) => setEmail(e.target.value)}
+            maxLength={EMAIL_MAX_LENGTH}
+          />
+          <StButton
+            disabled={onDupEmailDisableHandler()}
+            onClick={() => {
+              onEmailDupCheckHandler(email);
+            }}
+          >
+            중복체크
+          </StButton>
+        </StDiv>
+
+        <StInput
+          type="password"
+          placeholder="비밀번호"
+          onChange={(e) => setPw(e.target.value)}
+          maxLength={PW_MAX_LENGTH}
+        />
+        {passwordRegCheck(pw) ? (
+          <p className="pass">형식에 맞는 비밀번호 입니다.</p>
+        ) : (
+          <p className="danger"> 영문+숫자 8-20자로 만들어 주세요.</p>
+        )}
+
+        <StInput
+          type="password"
+          placeholder="비밀번호 재확인"
+          onChange={(e) => setPwTwo(e.target.value)}
+          maxLength={PW_MAX_LENGTH}
+        />
+        {pw === pwTwo && pwTwo?.length > PW_MIN_LENGTH ? (
+          <p className="pass">형식에 맞는 비밀번호 입니다.</p>
+        ) : (
+          <p className="danger"> 비밀번호가 일치하지 않거나 공백입니다.</p>
+        )}
+
+        <StSubmitButton
+          as="button"
+          onClick={onSignUpHandler}
+          disabled={onDisableHandler()}
+        >
+          가입하기
+        </StSubmitButton>
+      </StWrap>
+    </>
+  );
+};
+
+export default Signup;
--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,14 +1,15 @@
-import React, { useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
-import flex from '../Components/Common/flex'
-import Mainheader from '../Components/MainComponents/Mainheader'
-import { __addPost } from '../Redux/modules/posting'
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import flex from "../Components/Common/flex";
+import Mainheader from "../Components/MainComponents/Mainheader";
+import { __addPost } from "../Redux/modules/posting";
 import { useDispatch } from "react-redux";
-import { storage } from '../Shared/firebase'
-import {getDownloadURL, ref, uploadBytes} from 'firebase/storage'
+import { storage } from "../Shared/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
-const UPLOAD_IMG = "https://t3.ftcdn.net/jpg/02/70/22/86/360_F_270228625_yujevz1E4E45qE1mJe3DyyLPZDmLv4Uj.jpg"
+const UPLOAD_IMG =
+  "https://t3.ftcdn.net/jpg/02/70/22/86/360_F_270228625_yujevz1E4E45qE1mJe3DyyLPZDmLv4Uj.jpg";
 
 const Post = () => {
   const nameRef = useRef();
@@ -41,50 +42,54 @@ const Post = () => {
   const [menuTwo, setMenuTwo] = useState(UPLOAD_IMG);
   const [menuThree, setMenuThree] = useState(UPLOAD_IMG);
 
-   // 파일 저장
-   const saveFileImage = (e, imgRef, set) => {
+  // 파일 저장
+  const saveFileImage = (e, imgRef, set) => {
     set(URL.createObjectURL(e.target.files[0]));
-    uploadFB(e, imgRef)
-   };
-   
+    uploadFB(e, imgRef);
+  };
+
   const uploadFB = async (e, imgRef) => {
-      const upload_file = await uploadBytes(ref(storage, `images/${e.target.files[0].name}`),
-      e.target.files[0])
-      const file_url = await getDownloadURL(upload_file.ref);
-      imgRef.current = {url:file_url};
+    const upload_file = await uploadBytes(
+      ref(storage, `images/${e.target.files[0].name}`),
+      e.target.files[0]
+    );
+    const file_url = await getDownloadURL(upload_file.ref);
+    imgRef.current = { url: file_url };
   };
 
   const onPostHandler = () => {
-   try{
-    dispatch(__addPost({
-      name: nameRef.current.value,
-      location: locationRef.current.value,
-      phone: phoneRef.current.value,
-      logoImg: logoImgRef.current.url,
-      openingHours: openingHoursRef.current.value,
-      minPrice: minPriceRef.current.value,
+    try {
+      dispatch(
+        __addPost({
+          name: nameRef.current.value,
+          location: locationRef.current.value,
+          phone: phoneRef.current.value,
+          logoImg: logoImgRef.current.url,
+          openingHours: openingHoursRef.current.value,
+          minPrice: minPriceRef.current.value,
 
-      menuName: menuNameRef.current.value,
-      price: priceRef.current.value,
-      explain: explainRef.current.value,
-      menuImg: menuImgRef.current.url,
+          menuName: menuNameRef.current.value,
+          price: priceRef.current.value,
+          explain: explainRef.current.value,
+          menuImg: menuImgRef.current.url,
 
-      menuNameTwo: twomenuNameRef.current.value,
-      priceTwo: twopriceRef.current.value,
-      explainTwo: twoexplainRef.current.value,
-      menuImgTwo: twomenuImgRef.current.url,
+          menuNameTwo: twomenuNameRef.current.value,
+          priceTwo: twopriceRef.current.value,
+          explainTwo: twoexplainRef.current.value,
+          menuImgTwo: twomenuImgRef.current.url,
 
-      menuNameThree: threemenuNameRef.current.value,
-      priceThree: threepriceRef.current.value,
-      explainThree: threeexplainRef.current.value,
-      menuImgThree: threemenuImgRef.current.url,
-    }))
-    alert('등록이 완료되었습니다')
-    navigate('/')
-   }catch(e){
-    alert("사진을 선택해 주셔야합니다.");
-   }
-  }
+          menuNameThree: threemenuNameRef.current.value,
+          priceThree: threepriceRef.current.value,
+          explainThree: threeexplainRef.current.value,
+          menuImgThree: threemenuImgRef.current.url,
+        })
+      );
+      alert("등록이 완료되었습니다");
+      navigate("/");
+    } catch (e) {
+      alert("사진을 선택해 주셔야합니다.");
+    }
+  };
 
   return (
     <>
@@ -92,89 +97,191 @@ const Post = () => {
       <StWrap>
         <StDiv>
           <StList>
-          <img style={{width:'90%', height:'150px'}} src={fileImage} alt="" />
-          <input type="file" placeholder='PICTURE' onChange={(e)=>saveFileImage(e,logoImgRef, setFileImage)} required/>
+            <img
+              style={{ width: "90%", height: "150px" }}
+              src={fileImage}
+              alt=""
+            />
+            <input
+              type="file"
+              placeholder="PICTURE"
+              onChange={(e) => saveFileImage(e, logoImgRef, setFileImage)}
+              required
+            />
           </StList>
           <StList>
-            <input type="text" placeholder='가게 이름' maxLength={11} ref={nameRef}/>
-            <input type="text" placeholder='장소 예) 영등포점' maxLength={10} ref={locationRef}/>
-            <input type="number" placeholder='전화번호 예) 07012345678' maxLength={11} ref={phoneRef}/>
-            <input type="text" placeholder='오프시간 예) 오전11시' maxLength={6} ref={openingHoursRef}/>
-            <input type="text" placeholder='최소 주문 금액 예) 20000' maxLength={7} ref={minPriceRef}/>
+            <input
+              type="text"
+              placeholder="가게 이름"
+              maxLength={11}
+              ref={nameRef}
+            />
+            <input
+              type="text"
+              placeholder="장소 예) 영등포점"
+              maxLength={10}
+              ref={locationRef}
+            />
+            <input
+              type="number"
+              placeholder="전화번호 예) 07012345678"
+              maxLength={11}
+              ref={phoneRef}
+            />
+            <input
+              type="text"
+              placeholder="오프시간 예) 오전11시"
+              maxLength={6}
+              ref={openingHoursRef}
+            />
+            <input
+              type="text"
+              placeholder="최소 주문 금액 예) 20000"
+              maxLength={7}
+              ref={minPriceRef}
+            />
           </StList>
           <StList>
-          <img style={{width:'90%', height:'150px'}} src={menuOne} alt="" />
+            <img
+              style={{ width: "90%", height: "150px" }}
+              src={menuOne}
+              alt=""
+            />
           </StList>
           <StList>
-          <input type="file" placeholder='PICTURE' onChange={(e)=>saveFileImage(e,menuImgRef, setMenuOne)} required/>
-            <input type="text" placeholder='메뉴 이름(최대10자)' maxLength={15} ref={menuNameRef}/>
-            <input type="number" placeholder='가격 예) 30000' maxLength={6} ref={priceRef}/>
-            <input type="text" placeholder='설명 (최대20자)' maxLength={20} ref={explainRef}/>
+            <input
+              type="file"
+              placeholder="PICTURE"
+              onChange={(e) => saveFileImage(e, menuImgRef, setMenuOne)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="메뉴 이름(최대10자)"
+              maxLength={15}
+              ref={menuNameRef}
+            />
+            <input
+              type="number"
+              placeholder="가격 예) 30000"
+              maxLength={6}
+              ref={priceRef}
+            />
+            <input
+              type="text"
+              placeholder="설명 (최대20자)"
+              maxLength={20}
+              ref={explainRef}
+            />
           </StList>
           <StList>
-          <img style={{width:'90%', height:'150px'}} src={menuTwo} alt="" />
+            <img
+              style={{ width: "90%", height: "150px" }}
+              src={menuTwo}
+              alt=""
+            />
           </StList>
           <StList>
-          <input type="file" placeholder='PICTURE' onChange={(e)=>saveFileImage(e,twomenuImgRef, setMenuTwo)} required/>
-            <input type="text" placeholder='메뉴 이름(최대10자)' maxLength={15} ref={twomenuNameRef}/>
-            <input type="text" placeholder='가격 예) 30000' maxLength={10} ref={twopriceRef}/>
-            <input type="text" placeholder='설명 (최대20자)' maxLength={20} ref={twoexplainRef}/>
+            <input
+              type="file"
+              placeholder="PICTURE"
+              onChange={(e) => saveFileImage(e, twomenuImgRef, setMenuTwo)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="메뉴 이름(최대10자)"
+              maxLength={15}
+              ref={twomenuNameRef}
+            />
+            <input
+              type="text"
+              placeholder="가격 예) 30000"
+              maxLength={10}
+              ref={twopriceRef}
+            />
+            <input
+              type="text"
+              placeholder="설명 (최대20자)"
+              maxLength={20}
+              ref={twoexplainRef}
+            />
           </StList>
           <StList>
-          <img style={{width:'90%', height:'150px'}} src={menuThree} alt="" />
+            <img
+              style={{ width: "90%", height: "150px" }}
+              src={menuThree}
+              alt=""
+            />
           </StList>
           <StList>
-          <input type="file" placeholder='PICTURE' onChange={(e)=>saveFileImage(e,threemenuImgRef, setMenuThree)} required/>
-            <input type="text" placeholder='메뉴 이름(최대10자)' maxLength={15} ref={threemenuNameRef}/>
-            <input type="number" placeholder='가격 예) 30000' maxLength={10} ref={threepriceRef}/>
-            <input type="text" placeholder='설명 (최대20자)' maxLength={20} ref={threeexplainRef}/>
+            <input
+              type="file"
+              placeholder="PICTURE"
+              onChange={(e) => saveFileImage(e, threemenuImgRef, setMenuThree)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="메뉴 이름(최대10자)"
+              maxLength={15}
+              ref={threemenuNameRef}
+            />
+            <input
+              type="number"
+              placeholder="가격 예) 30000"
+              maxLength={10}
+              ref={threepriceRef}
+            />
+            <input
+              type="text"
+              placeholder="설명 (최대20자)"
+              maxLength={20}
+              ref={threeexplainRef}
+            />
           </StList>
         </StDiv>
-        <StButton 
-          onClick={onPostHandler}
-        >
-          추가하기
-        </StButton>
+        <StButton onClick={onPostHandler}>추가하기</StButton>
       </StWrap>
     </>
-  )
-}
-export default Post
+  );
+};
+export default Post;
 
 const StWrap = styled.div`
-    ${flex({direction:'column',align:'center'})}
-    width: 100%;
-    height: 100%;
-    background-color: var(--white);
+  ${flex({ direction: "column", align: "center" })}
+  width: 100%;
+  height: 100%;
+  background-color: var(--white);
 `;
 
 const StDiv = styled.div`
-    ${flex({direction:'column',justify:'flex-start'})}
-    width: calc(100vh - 60vh);
-    height: 650px;
-    overflow-y: scroll;
-    & > ul{
-        margin-bottom: 1rem;
-    }
+  ${flex({ direction: "column", justify: "flex-start" })}
+  width: calc(100vh - 60vh);
+  height: 650px;
+  overflow-y: scroll;
+  & > ul {
+    margin-bottom: 1rem;
+  }
 `;
 
 const StList = styled.div`
-    ${flex({direction:'column'})}
-    width: calc(100vh - 60vh);
-    min-height: 200px;
+  ${flex({ direction: "column" })}
+  width: calc(100vh - 60vh);
+  min-height: 200px;
+  border-bottom: 1px solid var(--font-secondary);
+  font-size: 1.5rem;
+  & > input {
+    border: none;
     border-bottom: 1px solid var(--font-secondary);
-    font-size: 1.5rem;
-    & > input{
-      border: none;
-      border-bottom: 1px solid var(--font-secondary);
-      margin-bottom: 1rem;
-    }
+    margin-bottom: 1rem;
+  }
 `;
 
 const StButton = styled.button`
-    width: 200px;
-    margin-top: 1rem;
-    padding: 10px;
-    border-radius: 30px;
-    font-size: 2rem;
+  width: 200px;
+  margin-top: 1rem;
+  padding: 10px;
+  border-radius: 30px;
+  font-size: 2rem;
 `;
